6d498944ae35a2449a4f60e806e243476efc466f
shijianlei <shijianlei.sjl@sunyur.com>
2023-10-20 10:33:34 +0800

    新项目

---
+++ src/main/java/com/github/springboot/git/commond/SpringBootGitCommondApplication.java
@@ -0,0 +1,13 @@
+package com.github.springboot.git.commond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGitCommondApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGitCommondApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/springboot/git/commond/SpringBootGitCommondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.springboot.git.commond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGitCommondApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
