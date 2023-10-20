13943dde5780201e253454fef19345bba34c9a9a
shijianlei <shijianlei.sjl@sunyur.com>
2023-10-20 11:33:02 +0800

    model raname

--- src/main/java/com/github/springboot/git/commond/SpringBootGitCommandApplication.java
+++ src/main/java/com/github/springboot/git/commond/SpringBootGitCommandApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootGitCommondApplication {
+public class SpringBootGitCommandApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootGitCommondApplication.class, args);
+        SpringApplication.run(SpringBootGitCommandApplication.class, args);
     }
 
 }
